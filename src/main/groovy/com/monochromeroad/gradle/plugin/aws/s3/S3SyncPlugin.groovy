20c01f9a854f32ae336a89b7fa40bdb5d63de9db
literalice <literalice@monochromeroad.com>
2014-10-23 10:27:55 +0900

    adds publish build settings

---
+++ src/main/groovy/com/monochromeroad/gradle/plugin/aws/s3/S3SyncPlugin.groovy
@@ -0,0 +1,16 @@
+package com.monochromeroad.gradle.plugin.aws.s3
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * The plugin class for S3 sync
+ */
+class S3SyncPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+
+}
